f2a51e44ac22a87779b162fb1daa090df63b4609
Raj Yadav <rajyadav751811@gmail.com>
2024-06-18 11:09:51 +0530

    Eureka Server Project

--- 01_Service_Registry/src/main/java/in/raj/Application.java
+++ 01_Service_Registry/src/main/java/in/raj/Application.java
@@ -3,11 +3,9 @@ package in.raj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
