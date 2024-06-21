639968747ce4c2ca49f57b44ad95cf4396633620
Raj Yadav <rajyadav751811@gmail.com>
2024-06-21 07:41:31 +0530

    API GATEWAYS and FILTER

---
+++ 05_Api_Gateway/src/main/java/in/raj/filter/MyFilter.java
@@ -0,0 +1,26 @@
+package in.raj.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+
+@Component
+public class MyFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("Filter executed.......");
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
+        Set<String> keySet = headers.keySet();
+        for (String key : keySet) {
+            System.out.println(key + ": " + headers.getValuesAsList(key));
+        }
+        return chain.filter(exchange);
+    }
+}
